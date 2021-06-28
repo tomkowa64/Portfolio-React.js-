cd35e046dd1b00e5e6d89f599bd4725f49c27314
Tomasz Kowalski <tomkowa64@gmail.com>
2021-06-28 20:15:20 +0200

    Projects Section Early Design

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -3,14 +3,26 @@ import '../css/Projects.scss';
 
 //Navbar bootstrap components
 import {Container} from 'react-bootstrap';
+import {Col} from 'react-bootstrap';
 import {Row} from 'react-bootstrap';
 
 function Projects() {
   return (
     <section className="projects" id="projects">
         <Container fluid={true}>
-          <Row>
-            
+          <Row className="projects-group">
+            <Col sm="3" className="project-bg" id="project1">
+              1
+            </Col>
+            <Col sm="3" className="project-bg" id="project2">
+              2
+            </Col>
+            <Col sm="3" className="project-bg" id="project3">
+              3
+            </Col>
+            <Col sm="3" className="project-bg" id="project4">
+              4
+            </Col>
           </Row>
         </Container>
     </section>
