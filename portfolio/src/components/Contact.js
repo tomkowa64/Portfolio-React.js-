79f76780aebbfbddde08da24c30ac7023c29f818
Tomasz Kowalski <tomkowa64@gmail.com>
2021-07-16 10:11:57 +0200

    Changes in Contact section's inputs && Contact info added

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -7,9 +7,14 @@ import '../css/Contact.scss';
 //Navbar bootstrap components
 import {Container} from 'react-bootstrap';
 import {Row} from 'react-bootstrap';
+import {Col} from 'react-bootstrap'
 
 //Icons import from react bootstrap
 import {Envelope} from 'react-bootstrap-icons';
+import {TelephoneFill} from 'react-bootstrap-icons';
+import {EnvelopeFill} from 'react-bootstrap-icons';
+import {Globe2} from 'react-bootstrap-icons';
+import {HouseFill} from 'react-bootstrap-icons';
 
 class Contact extends React.Component {
   constructor(props) {
@@ -52,21 +57,32 @@ render(){
     <section className="contact" id="contact">
         <div className="section-label"><Envelope /></div>
         <Container fluid={true}>
-          <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
-            <div className="form-group">
-                <label htmlFor="name">Imię i Nazwisko</label>
-                <input type="text" className="form-control" />
-            </div>
-            <div className="form-group">
-                <label htmlFor="exampleInputEmail1">Email</label>
-                <input type="email" className="form-control" aria-describedby="emailHelp" />
-            </div>
-            <div className="form-group">
-                <label htmlFor="message">Wiadomość</label>
-                <textarea className="form-control" rows="5"></textarea>
-            </div>
-            <button type="submit" className="btn btn-primary">Wyślij</button>
-          </form>
+          <Row>
+            <Col sm="7">
+              <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+                <div className="form-group">
+                    <label htmlFor="name">Imię i Nazwisko</label>
+                    <input type="text" className="form-control" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">Email</label>
+                    <input type="email" className="form-control" aria-describedby="emailHelp" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="message">Wiadomość</label>
+                    <textarea className="form-control" rows="8"></textarea>
+                </div>
+                <button type="submit" className="btn btn-primary">Wyślij</button>
+              </form>
+            </Col>
+            <Col sm="5" className="contact-about">
+              <p className="text-center">Dane kontaktowe</p>
+              <p><TelephoneFill /><span className="contact-about-text">+48 794 682 811</span></p>
+              <p><EnvelopeFill/><span className="contact-about-text">tomkowa64@gmail.com</span></p>
+              <p><Globe2/><span className="contact-about-text">www.sampleWebsiteLink.pl</span></p>
+              <p><HouseFill /><span className="contact-about-text">Rzeszów, ul. prof. Stanisława Pigonia 1, s. 333 </span></p>
+            </Col>
+          </Row>  
         </Container>
     </section>
   );
